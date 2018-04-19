6d7d2633cd17921d34d8c5346c4027250c8af2c0
Antonio Calvo <calvocalvoantonio@hotmail.com>
2018-04-19 12:33:02 +0200

    In this class we will implement and resolve the population problem

---
+++ src/ingproy/GrowthRepresenter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ingproy;
+
+/**
+ *
+ * @author antoniocalvocalvo
+ */
+//In this class we will implement and resolve the population problem
+public class GrowthRepresenter extends MainWin{
+
+    public GrowthRepresenter(int winWidth, int winLength, String title) {
+        super(winWidth, winLength, title);
+    }
+
+  
+    
+    
+    
+}
