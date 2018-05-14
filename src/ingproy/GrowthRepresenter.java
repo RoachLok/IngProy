cefae3de7c9925223216aa03afa353b3f250b703
Antonio Calvo <calvocalvoantonio@hotmail.com>
2018-05-14 09:32:57 +0200

    Nuevos caambios.Taborda arregla para que salga el panel principal con las 3 ventanas primero¡¡

--- src/ingproy/GrowthRepresenter.java
+++ src/ingproy/GrowthRepresenter.java
@@ -5,6 +5,9 @@
  */
 package ingproy;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import javax.swing.*;
 
 /**
@@ -22,38 +25,100 @@ public class GrowthRepresenter extends MainWin{
         super(winWidth, winLength, title, widthPos, lengthPos);
     }
     
-    /*
+   JPanel test = new JPanel ();
     
-    IMPORTANTE: NO TE VA A COMPILAR PORQUE HE AÑADIDO MÉTODOS
-                ABSTRACTOS A MAINWIN Y HASTA QUE NO LOS 
-                IMPLEMENTES NO TE VA A DEJAR COMPILAR.
-                Puedes añadirlos dándole a la bombilla de al lado del constructor,
-                implement all abstract methods.
-    
-    */
-
-    JPanel test = new JPanel ();
-    
-    @Override
-    JComponent input() {
+ JPanel input() {
+        //Creates the "holder" Panel with X-Axis boxlayout. 
+        JPanel paletPanel = new JPanel ();
+        paletPanel.setLayout(new BoxLayout(paletPanel, BoxLayout.Y_AXIS));
+        paletPanel.setBorder(BorderFactory.createCompoundBorder(
+                                    BorderFactory.createTitledBorder("= Representación crecimiento poblaciones ="),
+                                        BorderFactory.createEmptyBorder(5,5,5,5)));
+        paletPanel.setBackground(Color.WHITE);
         
-        return test;
-    }
-
-    @Override
+        //"Spacer"
+        JLabel blankSpace = new JLabel (" ");
+        blankSpace.setBackground(Color.WHITE);
+        
+        //Eq. input field.
+        JPanel inputSection = new JPanel ();
+        inputSection.setLayout(new FlowLayout(3));
+        
+        JTextField inputText = new JTextField ("10000", 10); //aqui 
+        JTextField inputText2 = new JTextField ("3", 10);
+        JTextField inputTex3 = new JTextField ("10", 10);
+        JTextField inputTex4 = new JTextField ("40", 10);
+        Dimension minimumSize = new Dimension(100, 50);
+        inputText.setMinimumSize(minimumSize);
+        JLabel limite = new JLabel ("Limite popablación (k): ");
+        JLabel inicial = new JLabel (" Población Inicial ");
+        JLabel natalidad = new JLabel ("Tasa natalidad ");
+        JLabel generaciones = new JLabel ("Generaciones");
+        
+        limite.setLabelFor(inputText);
+        inputSection.add(limite);
+        inputSection.add(inputText);
+        
+        inicial.setLabelFor(inputText);
+        inputSection.add(inicial);
+        inputSection.add(inputText);
+       
+        natalidad.setLabelFor(inputText);
+        inputSection.add(natalidad);
+        inputSection.add(inputText);
+        
+        generaciones.setLabelFor(inputText);
+        inputSection.add(generaciones);
+        inputSection.add(inputText);
+        
+        // HACER UN ARRAY QUE CONTENGA A LOS COMPONENTES Y LES PONGA EL COLOR.
+        inputSection.setBackground(Color.WHITE);
+        inputText.setBackground(Color.GRAY);
+        limite.setBackground(Color.WHITE);
+        
+        inputSection.setBackground(Color.WHITE);
+        inputText.setBackground(Color.GRAY);
+        inicial.setBackground(Color.WHITE);
+        
+        inputSection.setBackground(Color.WHITE);
+        inputText.setBackground(Color.GRAY);
+        natalidad.setBackground(Color.WHITE);
+        
+        inputSection.setBackground(Color.WHITE);
+        inputText.setBackground(Color.GRAY);
+        generaciones.setBackground(Color.WHITE);
+        
+        
+        //Adds it to the paletPanel.
+        paletPanel.add(blankSpace);
+        paletPanel.add(inputSection);
+        
+        return paletPanel;
+    }   
+      
+  @Override
     JComponent output() {
+        
+        
+        
         
         return test;
     }
 
     @Override
     JComponent pastOutput() {
+       int poblacionFinal;
+       
+      
+        
+        
+
+       // poblacion final = (inicial * natalidad);
+      //  *genraciones maximo de limite poblacion;
+        
         
         return test;
     }
-    
-    
-
   
     
     
