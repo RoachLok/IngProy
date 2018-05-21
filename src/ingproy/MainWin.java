b6ed6ed13bda0e79b558eb743fd1e923d3e0ff99
RoachLok <jtabordajunior@hotmail.com>
2018-05-21 17:36:38 +0200

    Converted MainWin into a JFrame to fit the MainMenu tabbed system.

--- src/ingproy/MainWin.java
+++ src/ingproy/MainWin.java
@@ -15,31 +15,33 @@ import java.awt.*;
  *
  * @author jtabo_000
  */
-    abstract class MainWin extends JFrame implements ActionListener{
+abstract class MainWin extends JPanel implements ActionListener {
+
     private final String newLine = "/n";
-    
-    public MainWin (int winWidth, int winLength, String title){
+
+    public MainWin(int winWidth, int winLength, String title) {
         // Sets window's width and length, and spawning location.
         
-        
-        setTitle(title);
-        setSize(winWidth, winLength);
-        setLocationRelativeTo(null);
-        
+        setLayout(new BorderLayout());
+
+        //setTitle(title);
+        //setSize(winWidth, winLength);
+        //setLocationRelativeTo(null);
+
         // Generates main JPanel
-        JPanel primaryPanel = new JPanel ();
-        primaryPanel.setLayout(new BoxLayout (primaryPanel, BoxLayout.X_AXIS));
+        JPanel primaryPanel = new JPanel();
+        primaryPanel.setLayout(new BoxLayout(primaryPanel, BoxLayout.X_AXIS));
         primaryPanel.setBackground(Color.RED);
-        
+
         // Inserts on the main JPanel two Panels, one JPanel on the left and one JScrollerPane on the right.
-        JPanel leftPanel = new JPanel ();
-        leftPanel.setLayout (new BoxLayout (leftPanel, BoxLayout.Y_AXIS));
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
         leftPanel.setBackground(Color.MAGENTA);
-        JScrollPane outputScroller = new JScrollPane (output());
-        
+        JScrollPane outputScroller = new JScrollPane(output());
+
         outputScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         outputScroller.setBackground(Color.RED);
-        
+
         //Sets a splitter between leftPanel and outputScroller by adding them to a JSplitPanel.
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, outputScroller);
         splitPane.setOneTouchExpandable(true);
@@ -50,53 +52,56 @@ import java.awt.*;
         Dimension minimumSize = new Dimension(100, 50);
         leftPanel.setMinimumSize(minimumSize);
         outputScroller.setMinimumSize(minimumSize);
-        
+
         //Adds the splitPane to the primaryPanel
         primaryPanel.add(splitPane);
-        
+
         //Inserts to the Left JPanel an upper JPanel and a lower JScrollerPane.
-        JPanel inputPanel = new JPanel ();
+        JPanel inputPanel = new JPanel();
         inputPanel.setLayout(new GridLayout());
         inputPanel.setBackground(Color.YELLOW);
-        
+
         JScrollPane explorerScrollPane = new JScrollPane(pastOutput());
         explorerScrollPane.setBackground(Color.darkGray);
-        
+
         //Sets a splitter between inputPanel and explorerScrollPane.
         JSplitPane splitPane2 = new JSplitPane(JSplitPane.VERTICAL_SPLIT, inputPanel, explorerScrollPane);
         splitPane2.setOneTouchExpandable(true);
-        splitPane2.setResizeWeight (0.5);
-        
+        splitPane2.setResizeWeight(0.5);
+
         //Allows to insert a JComponent inside inputPanel.
         inputPanel.add(input());
-        
+
         //Adds the JSplitPane to the left JPanel
         leftPanel.add(splitPane2);
-        
-        add (primaryPanel);
-        
+
+        add(primaryPanel);
+
     }
-    
-    public MainWin (int winWidth, int winLength, String title, int widthPos, int lengthPos){
+
+    public MainWin(int winWidth, int winLength, String title, int widthPos, int lengthPos) {
         // Sets window's width and length, and spawning location.
-        setTitle(title);
-        setSize(winWidth, winLength);
-        setLocation(widthPos,lengthPos);
         
+        setLayout(new BorderLayout());
+        
+        //setTitle(title);
+        //setSize(winWidth, winLength);
+        //setLocation(widthPos, lengthPos);
+
         // Generates main JPanel
-        JPanel primaryPanel = new JPanel ();
-        primaryPanel.setLayout(new BoxLayout (primaryPanel, BoxLayout.X_AXIS));
+        JPanel primaryPanel = new JPanel();
+        primaryPanel.setLayout(new BoxLayout(primaryPanel, BoxLayout.X_AXIS));
         primaryPanel.setBackground(Color.RED);
-        
+
         // Inserts on the main JPanel two Panels, one JPanel on the left and one JScrollerPane on the right.
-        JPanel leftPanel = new JPanel ();
-        leftPanel.setLayout (new BoxLayout (leftPanel, BoxLayout.Y_AXIS));
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
         leftPanel.setBackground(Color.MAGENTA);
-        JScrollPane outputScroller = new JScrollPane (output());
-        
+        JScrollPane outputScroller = new JScrollPane(output());
+
         outputScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         outputScroller.setBackground(Color.RED);
-        
+
         //Sets a splitter between leftPanel and outputScroller by adding them to a JSplitPanel.
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, outputScroller);
         splitPane.setOneTouchExpandable(true);
@@ -107,60 +112,51 @@ import java.awt.*;
         Dimension minimumSize = new Dimension(100, 50);
         leftPanel.setMinimumSize(minimumSize);
         outputScroller.setMinimumSize(minimumSize);
-        
+
         //Adds the splitPane to the primaryPanel
         primaryPanel.add(splitPane);
-        
+
         //Inserts to the Left JPanel an upper JPanel and a lower JScrollerPane.
-        JPanel inputPanel = new JPanel ();
+        JPanel inputPanel = new JPanel();
         inputPanel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.anchor = GridBagConstraints.FIRST_LINE_START;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        
+
         inputPanel.setBackground(Color.YELLOW);
         JScrollPane explorerScrollPane = new JScrollPane(pastOutput());
         explorerScrollPane.setBackground(Color.darkGray);
-        
+
         //Sets a splitter between inputPanel and explorerScrollPane.
         JSplitPane splitPane2 = new JSplitPane(JSplitPane.VERTICAL_SPLIT, inputPanel, explorerScrollPane);
         splitPane2.setOneTouchExpandable(true);
-        splitPane2.setResizeWeight (0.5);
-        
+        splitPane2.setResizeWeight(0.5);
+
         //Allows to insert a JComponent inside inputPanel.
         inputPanel.add(input(), gbc);
-        
+
         //Adds the JSplitPane to the left JPanel
         leftPanel.add(splitPane2);
-        
-        add (primaryPanel);
-        
+
+        add(primaryPanel);
+
     }
-    
-    
+
     //Abstract methods that all windows should have. Further documentation will be added.
-  
-    abstract JComponent input ();
+    abstract JComponent input();
     // Top-Left Panel.
-    
-    abstract JComponent output ();
+
+    abstract JComponent output();
     // Right Panel.
-    
-    abstract JComponent pastOutput ();
+
+    abstract JComponent pastOutput();
     // Bottom-Left Panel.
-    
-    
-    
-    
-    
-    
-    
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
 
 /*FlowLayout layout = new FlowLayout();
